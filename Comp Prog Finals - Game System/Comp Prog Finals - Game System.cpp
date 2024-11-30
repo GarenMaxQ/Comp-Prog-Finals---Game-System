ecf6d3f7ed121671184e4ace99cdb122702b7c0b
Daniel <ayanokoujik81@gmail.com>
2024-11-30 08:49:46 +0800

    Di pa tapos

--- Comp Prog Finals - Game System/Comp Prog Finals - Game System.cpp
+++ Comp Prog Finals - Game System/Comp Prog Finals - Game System.cpp
@@ -13,14 +13,147 @@ int main()
 	"Programming", "Science", "Philosophy", "P.E.",
 	"Music", "PPE"
 	*/
-	int hp = 100, mp = 50, phy = 30, mag = 20, gun = 10, bullets = 10;
+	int hp, mp, phy, mag, gun = 10, bullets = 10, energy;
 	int monsterHP, monsterMP, monsterPHY, monsterMAG, monster2HP, monster2MP, monster2PHY, monster2MAG;
 	string user, teammate1, teammate2, teammate3, monsterName, monster2Name;
-	char monsterChoice, actionChoice, fireAgain = false;
-	bool invalidMonster, invalidAction, repeatGun, invalidGun = false;
+	char monsterChoice, actionChoice, fireAgain = false, classChoice, itemChoice;
+	bool invalidMonster, invalidAction, repeatGun, invalidGun = false, invalidClass, invalidItem = false;
 	cout << "Enter your name: ";
 	getline(cin, user);
+	cout << "Choose starting class.\n";
+	cout << "1. Knight\n";
+	cout << "\tHealth: 300\n\tAtk Dmg: 20\n\tEnergy: 50\n ";
+	cout << "2. Sorcerer\n";
+	cout << "\tHealth: 150\n\tAtk Dmg: 50\n\tEnergy: 30\n";
+	cout << "3. Assassin\n";
+	cout << "\tHealth: 100\n\tAtk Dmg: 40\n\tEnergy 60\n";
+	cout << "Enter number of chosen class: ";
+	cin >> classChoice;
+	do {
+		switch (classChoice)
+		{
+		case'1':
+			hp = 300; phy = 20; energy = 50;
+			cout << "Starting Class: Knight\n";
+			invalidClass = false;
+			break;
+		case '2':
+			hp = 150; phy = 50; energy = 30;
+			cout << "Starting Class: Sorcerer\n";
+			invalidClass = false;
+			break;
+		case '3':
+			hp = 100; phy = 40; energy = 60;
+			cout << "Starting Class: Assassin\n";
+			invalidClass = false;
+			break;
+		default:
+			cout << "Not a valid class\n";
+			cout << "Choose starting class.\n";
+			cout << "1. Knight\n";
+			cout << "\tHealth: 300\n\tAtk Dmg: 20\n\tEnergy: 50\n ";
+			cout << "2. Sorcerer\n";
+			cout << "\tHealth: 150\n\tAtk Dmg: 50\n\tEnergy: 30\n";
+			cout << "3. Assassin\n";
+			cout << "\tHealth: 120\n\tAtk Dmg: 35\n\tEnergy 60\n";
+			cout << "Enter number of chosen class: ";
+			cin >> classChoice;
+			invalidClass = true;
+			break;
+		}
+	} while (invalidClass);
 
+	system("pause");
+	system("cls");
+
+	cout << "Level 1\n";
+	monsterHP = 50;
+	monsterName = "Xxr4mc3Lm@51k1pxX";
+	monsterPHY = 10;
+	do {
+	cout << "\nPlayer: " << user << "\tHealth: " << hp << "\tEnergy: " << energy << endl;
+	cout << "Monster: " << monsterName << "\tHealth: " << monsterHP << endl;
+	cout << "It's your turn.\n";
+	cout << "Select action.\n";
+	cout << "1. Attack\n";
+	cout << "2. Use item\n";
+	cout << "3. Do nothing\n";
+	cout << "Enter: ";
+	cin >> actionChoice;
+	do {
+		switch (actionChoice)
+		{
+		case'1':
+			monsterHP -= phy;
+			cout << "You attacked " << monsterName << "!" << endl;
+			cout << monsterName << " lost " << phy << " Health.\n";
+			cout << "Ending turn\n";
+			invalidAction = false;
+			break;
+		case '2':
+			cout << "Inventory\n";
+			cout << "1. PotPots - +20HP\n";
+			cout << "2. Return\n";
+			cout << "Enter: ";
+			cin >> itemChoice;
+			do {
+				switch (itemChoice)
+				{
+				case'1':
+					hp += 20;
+					cout << "Used PotPots! Gained 20HP.\n";
+					cout << "Ending turn\n";
+					invalidAction = false;
+					break;
+				case'2':
+					cout << "Select action.\n";
+					cout << "1. Attack\n";
+					cout << "2. Use item\n";
+					cout << "3. Do nothing\n";
+					cout << "Enter: ";
+					cin >> actionChoice;
+					invalidAction = true;
+					break;
+				default:
+					cout << "Invalid Item\n";
+					cout << "Inventory\n";
+					cout << "1. PotPots - +20HP\n";
+					cout << "Enter: ";
+					cin >> itemChoice;
+					invalidItem = true;
+					break;
+				}
+			} while (invalidItem);
+			break;
+		case'3':
+			cout << "Ending turn\n";
+			invalidAction = false;
+			break;
+		default:
+			cout << "Invalid action.";
+			cout << "1. Physical Attack\n";
+			cout << "2. Magic Attack\n";
+			cout << "3. Use gun\n";
+			cout << "4. Defend\n";
+			cout << "Enter: ";
+			cin >> actionChoice;
+			invalidAction = true;
+			break;
+		}
+	} while (invalidAction);
+
+	cout << monsterName << " is attacking!\n";
+	hp -= monsterPHY;
+	cout << user << " lost " << monsterPHY << "HP!\n";
+}while (hp != 0 && hp > 0 || monsterHP != 0 && monsterHP > 0);
+
+	cout << "You have defeated " << monsterName << "!\n";
+	cout << "Proceed to the next level\n";
+
+	system("pause");
+	system("cls");
+
+	/*
 	cout << "Select an enemy" << endl;
 	cout << "1. Xxr4mc3Lm@51k1pxX - Level: Basic\n";
 	cout << "2. [(@rG0$iN○m@LnW@g)] - Level: Intermediate\n";
@@ -161,5 +294,6 @@ int main()
 			}
 		} while (invalidAction);
 cout << "It's " << monsterName << " and " << monster2Name << " turn";
+*/
 
 }
